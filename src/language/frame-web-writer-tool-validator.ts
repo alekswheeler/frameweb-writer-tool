a9c85a2edf328f9f03eac095ca399d5b591d6f29
Alex Oliveira <57152476+alekswheeler@users.noreply.github.com>
2025-03-09 15:08:04 -0300

    adicionando os tiers

--- src/language/frame-web-writer-tool-validator.ts
+++ src/language/frame-web-writer-tool-validator.ts
@@ -1,5 +1,5 @@
 import type { ValidationAcceptor, ValidationChecks } from 'langium';
-import type { FrameWebWriterToolAstType, Person } from './generated/ast.js';
+import { ClassDef, FrameWebWriterToolAstType} from './generated/ast.js';
 import type { FrameWebWriterToolServices } from './frame-web-writer-tool-module.js';
 
 /**
@@ -9,7 +9,7 @@ export function registerValidationChecks(services: FrameWebWriterToolServices) {
     const registry = services.validation.ValidationRegistry;
     const validator = services.validation.FrameWebWriterToolValidator;
     const checks: ValidationChecks<FrameWebWriterToolAstType> = {
-        Person: validator.checkPersonStartsWithCapital
+        ClassDef: validator.checkClassStartsWithCapital
     };
     registry.register(checks, validator);
 }
@@ -19,13 +19,13 @@ export function registerValidationChecks(services: FrameWebWriterToolServices) {
  */
 export class FrameWebWriterToolValidator {
 
-    checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
-        if (person.name) {
-            const firstChar = person.name.substring(0, 1);
-            if (firstChar.toUpperCase() !== firstChar) {
-                accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
-            }
-        }
+    checkClassStartsWithCapital(classDef: ClassDef, accept: ValidationAcceptor): void {
+        // if (ClassDef.) {
+        //     const firstChar = ClassDef.name.substring(0, 1);
+        //     if (firstChar.toUpperCase() !== firstChar) {
+        //         accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
+        //     }
+        // }
     }
 
 }
