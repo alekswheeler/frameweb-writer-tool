0c66173a360be233c263a64467981b256be93484
aleks <aleks.vix@outlook.com>
2025-06-22 11:18:17 -0300

    UML package feature
    its not possible yeat to refer classes from differents packages.

--- src/cli/generator.ts
+++ src/cli/generator.ts
@@ -1,4 +1,4 @@
-import { Model, ClassDef, PackageDeclaration, Program } from '../language/generated/ast.js';
+import { Model, ClassDef, PackageDeclaration, Program, InheritanceRelation, RelationBlock, RelationType } from '../language/generated/ast.js';
 import { expandToNode, joinToNode, toString } from 'langium/generate';
 import * as fs from 'node:fs';
 import * as path from 'node:path';
@@ -33,18 +33,70 @@ export function generateMermaidMd(program: Program, filePath: string, destinatio
 
     let result = "classDiagram\n\n";
 
+    result += "namespace " + program.stmts[0].name + '{\n\n';
+
     let classesArray = program.stmts[0].classes;
 
     classesArray.forEach(element => {
-        result += `class ${element.name}\n`;
+        result += `class ${element.name}{\n`;
         // console.log("class", element.name);
         element.attributes.forEach(att => {
-            result += `${element.name} : +String ${att.name}\n`;
+            result += `+String ${att.name}\n`;
             // console.log(element.name, ": +String",att.name)
         });
         // console.log()
-        result += "\n";
+        result += "}\n";
     });
+
+    
+    result += '}\n';
+    let relationDefArray = program.stmts[0].relations;
+
+    relationDefArray.forEach(element => {
+        if(element.$type === InheritanceRelation){
+            const inhetance = element as InheritanceRelation;
+            const value = `${inhetance.to} <|-- ${inhetance.from}`;
+            result += value;
+        }
+        if(element.$type === RelationBlock){
+            let relationName = element.name;
+            let relationType = element.relationType as RelationType;
+            let relationTypeName = relationType.associationType;
+            
+            let relation = element as RelationBlock;
+            let relations = relation.relations;
+
+
+            if(relationTypeName === 'Composition'){
+                relations.forEach(x => {
+                    result += `${x.to} "${x.fromN}"  *-- "${x.toN}" ${x.from}`;
+                    if(relationName !== undefined){
+                        result += ` : ${relationName} \n`;
+                    }
+                });
+            }
+
+            if(relationTypeName === 'Agregation'){
+                relations.forEach(x => {
+                    result += `${x.to} "${x.fromN}" o-- "${x.toN}" ${x.from}`;
+                    if(relationName !== undefined){
+                        result += ` : ${relationName} \n`;
+                    }
+                });
+            }
+
+            if(relationTypeName === 'Association'){
+                relations.forEach(x => {
+                    result += `${x.to} "${x.fromN}" <-- "${x.toN}" ${x.from}`;
+                    if(relationName !== undefined){
+                        result += ` : ${relationName} \n`;
+                    }
+                });
+            } 
+        }
+
+    });
+
 
     fs.writeFileSync(generatedFilePath, result);
     return result;
